bc71a89441f9dbeb71e599edd0a55dfa6d12f0c0
zhaitao <370985116@qq.com>
2016-03-15 15:47:41 +0800

    内联函数

--- app/src/main/java/com/seniorzhai/kotlindemo/BaseActivity.kt
+++ app/src/main/java/com/seniorzhai/kotlindemo/BaseActivity.kt
@@ -1,6 +1,9 @@
 package com.seniorzhai.kotlindemo
 
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 
@@ -14,5 +17,19 @@ open class BaseActivity : AppCompatActivity() {
 
     fun Context.toast(text: Int, length: Int = Toast.LENGTH_SHORT) {
         Toast.makeText(this, text, length).show()
+    }
+
+    // 内联函数，会在编译的时候替换掉
+    inline fun supportsLollipop(code: () -> Unit) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            code()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportsLollipop {
+            window.statusBarColor = Color.BLUE
+        }
     }
 }
